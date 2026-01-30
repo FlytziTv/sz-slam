cd2f3e6a38b394e107d93f942806f62e2ea2c0c2
dealexis <flytzitv@gmail.com>
2026-01-30 13:46:03 +0100

    responsive header

--- components/sz/header.tsx
+++ components/sz/header.tsx
@@ -22,28 +22,30 @@ export default function Header() {
   const pathname = usePathname();
 
   return (
-    <header className="border-b border-border flex flex-row items-center gap-2 px-2 fixed top-0 left-0 right-0 bg-background z-10">
-      <Link href="/" className="p-2">
+    <header className="border-b border-border flex flex-row items-center justify-start gap-2 px-2 fixed top-0 left-0 right-0 bg-background z-10">
+      <Link href="/" className="shrink-0 z-10">
         <SZLogo size={30} />
       </Link>
-      {page.map((item) => {
-        const isActive = pathname === item.url;
-        return (
-          <Link
-            key={item.id}
-            href={item.url}
-            className={`group py-1.5 flex items-center justify-center text-foreground border-b transition-colors duration-150 ${
-              isActive ? "border-foreground" : "border-transparent"
-            }`}
-          >
-            <p
-              className={`group flex leading-none px-4 py-2 group-hover:bg-accent transition-colors duration-150 rounded-sm text-sm font-medium group-hover:text-foreground ${isActive ? "text-foreground" : "text-muted-foreground"}`}
+      <nav className="flex flex-row gap-1 overflow-x-auto scrollbar-none w-full h-full">
+        {page.map((item) => {
+          const isActive = pathname === item.url;
+          return (
+            <Link
+              key={item.id}
+              href={item.url}
+              className={`group py-1.5 flex items-center justify-center text-foreground border-b transition-colors duration-150 ${
+                isActive ? "border-foreground" : "border-transparent"
+              }`}
             >
-              {item.title}
-            </p>
-          </Link>
-        );
-      })}
+              <p
+                className={`group flex leading-none px-4 py-2 group-hover:bg-accent transition-colors duration-150 rounded-sm text-sm font-medium shrink-0 whitespace-nowrap group-hover:text-foreground ${isActive ? "text-foreground" : "text-muted-foreground"}`}
+              >
+                {item.title}
+              </p>
+            </Link>
+          );
+        })}
+      </nav>
     </header>
   );
 }
