ba5f8fc61f6e7348312662227530393b729e006d
dealexis <flytzitv@gmail.com>
2026-01-30 15:35:47 +0100

    responsive footer

--- components/sz/footer.tsx
+++ components/sz/footer.tsx
@@ -10,12 +10,12 @@ const footerlink = [
 export default function Footer() {
   return (
     <footer className="w-full border-t border-border bg-background mt-auto">
-      <div className="max-w-7xl mx-auto p-4 flex items-center justify-between">
-        <div className="flex flex-row gap-4">
+      <div className="max-w-7xl mx-auto p-4 flex flex-col md:flex-row items-center justify-between gap-4">
+        <div className="flex flex-col md:flex-row gap-4 items-center justify-center">
           <Link href="/" className="p-2">
             <SZLogo size={30} />
           </Link>
-          <div className="flex flex-row items-center justify-center gap-4">
+          <div className="flex flex-wrap items-center justify-center gap-4">
             {footerlink.map((item) => {
               return (
                 <Link
