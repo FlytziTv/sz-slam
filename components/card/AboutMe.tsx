6b1da686e82aab14d8ed97245137a95442ee3e1b
FlytziTv <flytzitv@gmail.com>
2026-02-05 08:53:32 +0100

    modif border

--- components/card/AboutMe.tsx
+++ components/card/AboutMe.tsx
@@ -4,13 +4,13 @@ import { SZInfos, SZInfosLink } from "../sz/SZInfos";
 export default function AboutMe() {
   return (
     <div className="group relative bg-card w-full border border-border rounded-md grid grid-cols-1 md:grid-cols-3">
-      <div className="flex flex-col gap-4 items-start p-4 border-b md:border-r border-border">
+      <div className="flex flex-col gap-4 items-start p-4 border-b md:border-0 md:border-r border-border">
         <SZInfos title="Prénom" value={personal_data.prenom} />
         <SZInfos title="Nom" value={personal_data.name} />
         <SZInfos title="Âge" value={personal_data.age} />
         <SZInfos title="Nationalité" value={personal_data.nationalite} />
       </div>
-      <div className="flex flex-col gap-4 p-4 border-b md:border-r border-border">
+      <div className="flex flex-col gap-4 p-4 border-b md:border-r md:border-0 border-border">
         <SZInfos title="Localisation" value={personal_data.location} />
         <SZInfos title="Disponibilité" value={personal_data.disponibilite} />
         <SZInfos title="Permis" value={personal_data.permis} />
